af07af18a9198a3d27b405bc0f7a05eebf3c49e6
Steven Cassidy <steven@mumsnet.com>
2015-12-30 15:26:58 -0500

    Add custom javascript

---
+++ app/assets/javascripts/bbb.js
@@ -0,0 +1,10 @@
+console.log('hello from bbb');
+
+function steven(dog, cat, mouse, fish) {
+  console.log('dog', dog);
+  console.log('cat', cat);
+  console.log('mouse', mouse); 
+  console.log('fish', fish); 
+}
+
+steven('rover', 'sylvester', 'mickey', 'dory');
